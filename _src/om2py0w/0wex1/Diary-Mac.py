642923c16c89d4e33964f74f156155004844730d
Iris-Di <dijian@dijiandeMacBook-Pro.local>
2015-10-21 21:23:01 +0800

    update diary in Mac

--- _src/om2py0w/0wex1/Diary-Mac.py
+++ _src/om2py0w/0wex1/Diary-Mac.py
@@ -4,11 +4,12 @@ timeStamp = int(time.time())
 timeArray = time.localtime(timeStamp)
 otherStyleTime = time.strftime("%Y/%m/%d %H:%M:%S", timeArray)
 
-txt = open('Diary.txt', 'r+')
 txt = open('Diary.txt', 'a+')
+txt = open('Diary.txt', 'r+')
 
 print "Here's your diary:"
 print txt.read()
+txt.close
 
 print "How are you today?" 
 content = raw_input(">")

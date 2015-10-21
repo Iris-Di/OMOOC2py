42c1d1e494805f51f453be66b17ce22702c5a4a7
Diggin <halfwayiris@gmail.com>
2015-10-21 14:24:46 +0800

    update Diary in Windows

--- _src/om2py0w/0wex1/Diary-Win.py
+++ _src/om2py0w/0wex1/Diary-Win.py
@@ -4,11 +4,11 @@ timeStamp = int(time.time())
 timeArray = time.localtime(timeStamp)
 otherStyleTime = time.strftime("%Y/%m/%d %H:%M:%S", timeArray)
 
-txt = open('Diary.txt', 'r')
+txt = open('Diary.txt')
 print "Here's your diary:"
 print txt.read()
 
-txt = open('Diary.txt', 'a')
+txt = open('Diary.txt', 'a+')
 print "How are you today?" 
 content = raw_input(">")
 
